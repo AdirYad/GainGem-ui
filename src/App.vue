12a2ef428c7f39aee936d83d3e10166efbd5b102
ortalll20111 <ortalll20111@gmail.com>
2020-11-11 15:30:08 +0200

    wip

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <template>
-  <div id="nav">
-    <router-link to="/">
-      <a src="/views/Home.vue">
-        <img class="logo" alt="logo" src="./assets/logo.png">
-      </a>
+  <div id="nav" class="tw-container tw-flex tw-m-auto tw-items-center">
+    <router-link :to="{name: 'Home'}">
+      <img class="logo" alt="logo" src="./assets/logo.png">
     </router-link>
-    <router-link to="/about">About</router-link>
+    <router-link :to="{name: 'About'}">About</router-link>
   </div>
 
   <router-view :key="$route.fullPath" />
