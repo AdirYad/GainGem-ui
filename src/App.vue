74825b84c3faf0c9da0df25c4a867ab17def55bc
ortalll20111 <ortalll20111@gmail.com>
2020-11-11 15:01:47 +0200

    add logo to home page

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
+    <router-link to="/">
+      <a src="/views/Home.vue">
+        <img class="logo" alt="logo" src="./assets/logo.png">
+      </a>
+    </router-link>
     <router-link to="/about">About</router-link>
   </div>
 
   <router-view :key="$route.fullPath" />
 </template>
-
+s
 <script>
 export default {
   name: 'App',
