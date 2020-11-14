796be815f5c49e96b20a097478049da9cabdde52
AdirYed <adir.yed@gmail.com>
2020-11-14 02:31:27 +0200

    deleted unnecessary use

--- src/main.js
+++ src/main.js
@@ -2,11 +2,9 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-import { VuelidatePlugin } from '@vuelidate/core';
 import AxiosPlugin, { axiosInstance } from '@/_helpers/axios';
 
 const app = createApp(App)
-    .use(VuelidatePlugin)
     .use(AxiosPlugin)
     .use(store)
     .use(router)
