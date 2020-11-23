dc85421820143b3d7ab823712500b07812d0fe0b
ortalll20111 <ortalll20111@gmail.com>
2020-11-23 15:33:54 +0200

    add rewards page

--- src/views/Rewards.vue
+++ src/views/Rewards.vue
@@ -5,49 +5,44 @@
         Rewards
       </div>
       <div class="rewards-list tw-flex tw-flex-wrap tw-items-center">
-        <div class="tw-px-3">
+        <div class="tw-p-3">
           <div class="reward reward-robux tw-flex tw-justify-center tw-items-center">
             <img class="img-height" src="../assets/images/rewards/robux.png" alt="robux">
           </div>
         </div>
-        <div class="tw-px-3">
+        <div class="tw-p-3">
           <div class="reward reward-bitcoin tw-flex tw-justify-center tw-items-center">
             <img src="../assets/images/rewards/bitcoin.png" alt="bitcoin">
           </div>
         </div>
-        <div class="tw-px-3">
-          <div class="reward reward-googleplay tw-flex tw-justify-center tw-items-center">
-            <img class="img-height" src="../assets/images/rewards/googleplay.png" alt="googleplay">
-          </div>
-        </div>
-        <div class="tw-px-3">
+        <div class="tw-p-3">
           <div class="reward reward-apple tw-flex tw-justify-center tw-items-center">
             <img src="../assets/images/rewards/apple.png" alt="apple">
           </div>
         </div>
-
-
-
-        <div class="tw-px-3">
-          <div class="reward reward-netflix tw-flex tw-justify-center tw-items-center">
-            <img src="../assets/images/rewards/netflix.png" alt="netflix">
-          </div>
-        </div>
-        <div class="tw-px-3">
+        <div class="tw-p-3">
           <div class="reward reward-xbox tw-flex tw-justify-center tw-items-center">
             <img src="../assets/images/rewards/xbox.png" alt="xbox">
           </div>
-
-
         </div>
-        <div class="reward-gap">
+        <div class="tw-p-3">
+          <div class="reward reward-roblox tw-flex tw-justify-center tw-items-center">
+            <img src="../assets/images/rewards/roblox.png" alt="roblox">
+          </div>
+        </div>
+        <div class="tw-p-3">
           <div class="reward reward-psn tw-flex tw-justify-center tw-items-center">
             <img src="../assets/images/rewards/psn.png" alt="psn">
           </div>
         </div>
-        <div class="reward-gap">
-          <div class="reward reward-roblox tw-flex tw-justify-center tw-items-center">
-            <img src="../assets/images/rewards/roblox.png" alt="roblox">
+        <div class="tw-p-3">
+          <div class="reward reward-googleplay tw-flex tw-justify-center tw-items-center">
+            <img class="img-height" src="../assets/images/rewards/googleplay.png" alt="googleplay">
+          </div>
+        </div>
+        <div class="tw-p-3">
+          <div class="reward reward-netflix tw-flex tw-justify-center tw-items-center">
+            <img src="../assets/images/rewards/netflix.png" alt="netflix">
           </div>
         </div>
       </div>
