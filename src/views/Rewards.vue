fcf1875d0a096ef66bdec688b87dac5e2b53d541
AdirYed <adir.yed@gmail.com>
2020-11-23 19:48:41 +0200

    out of stock badge

--- src/views/Rewards.vue
+++ src/views/Rewards.vue
@@ -6,15 +6,21 @@
       </div>
       <div class="rewards-list tw-grid">
         <router-link :to="{ name: 'Home' }" class="reward-item">
-          <div class="reward reward-robux tw-flex tw-justify-center tw-items-center">
+          <div class="reward reward-robux tw-relative tw-flex tw-justify-center tw-items-center">
+            <div v-if="true" class="tw-absolute badge-failed">
+                Out of stock
+            </div>
             <img src="@/assets/images/rewards/robux.png" alt="Robux">
           </div>
-          <div class="reward-details tw-flex tw-flex-col tw-justify-center tw-px-3 tw-font-medium tw-text-lg lg:tw-text-xl tw-uppercase tw-tracking-wider">
+          <div class="reward-details tw-flex tw-items-center tw-px-3 tw-font-medium tw-text-lg lg:tw-text-xl tw-uppercase tw-tracking-wider">
             Robux
           </div>
         </router-link>
         <router-link :to="{ name: 'Home' }" class="reward-item">
-          <div class="reward reward-bitcoin tw-flex tw-justify-center tw-items-center">
+          <div class="reward reward-bitcoin tw-relative tw-flex tw-justify-center tw-items-center">
+            <div v-if="false" class="tw-absolute badge-failed">
+              Out of stock
+            </div>
             <img src="@/assets/images/rewards/bitcoin.png" alt="Bitcoin">
           </div>
           <div class="reward-details tw-flex tw-flex-col tw-justify-center tw-px-3 tw-font-medium tw-text-lg lg:tw-text-xl tw-uppercase tw-tracking-wider">
@@ -22,7 +28,10 @@
           </div>
         </router-link>
         <router-link :to="{ name: 'Home' }" class="reward-item">
-          <div class="reward reward-apple tw-flex tw-justify-center tw-items-center">
+          <div class="reward reward-apple tw-relative tw-flex tw-justify-center tw-items-center">
+            <div v-if="false" class="tw-absolute badge-failed">
+              Out of stock
+            </div>
             <img src="@/assets/images/rewards/apple.png" alt="Appstore">
           </div>
           <div class="reward-details tw-flex tw-flex-col tw-justify-center tw-px-3 tw-font-medium tw-text-lg lg:tw-text-xl tw-uppercase tw-tracking-wider">
@@ -30,7 +39,10 @@
           </div>
         </router-link>
         <router-link :to="{ name: 'Home' }" class="reward-item">
-          <div class="reward reward-xbox tw-flex tw-justify-center tw-items-center">
+          <div class="reward reward-xbox tw-relative tw-flex tw-justify-center tw-items-center">
+            <div v-if="false" class="tw-absolute badge-failed">
+              Out of stock
+            </div>
             <img src="@/assets/images/rewards/xbox.png" alt="XBOX">
           </div>
           <div class="reward-details tw-flex tw-flex-col tw-justify-center tw-px-3 tw-font-medium tw-text-lg lg:tw-text-xl tw-uppercase tw-tracking-wider">
@@ -38,7 +50,10 @@
           </div>
         </router-link>
         <router-link :to="{ name: 'Home' }" class="reward-item">
-          <div class="reward reward-roblox tw-flex tw-justify-center tw-items-center">
+          <div class="reward reward-roblox tw-relative tw-flex tw-justify-center tw-items-center">
+            <div v-if="false" class="tw-absolute badge-failed">
+              Out of stock
+            </div>
             <img src="@/assets/images/rewards/roblox.png" alt="Roblox Giftcard">
           </div>
           <div class="reward-details tw-flex tw-flex-col tw-justify-center tw-px-3 tw-font-medium tw-text-lg lg:tw-text-xl tw-uppercase tw-tracking-wider">
@@ -46,7 +61,10 @@
           </div>
         </router-link>
         <router-link :to="{ name: 'Home' }" class="reward-item">
-          <div class="reward reward-psn tw-flex tw-justify-center tw-items-center">
+          <div class="reward reward-psn tw-relative tw-flex tw-justify-center tw-items-center">
+            <div v-if="false" class="tw-absolute badge-failed">
+              Out of stock
+            </div>
             <img src="@/assets/images/rewards/psn.png" alt="PSN">
           </div>
           <div class="reward-details tw-flex tw-flex-col tw-justify-center tw-px-3 tw-font-medium tw-text-lg lg:tw-text-xl tw-uppercase tw-tracking-wider">
@@ -54,7 +72,10 @@
           </div>
         </router-link>
         <router-link :to="{ name: 'Home' }" class="reward-item">
-          <div class="reward reward-googleplay tw-flex tw-justify-center tw-items-center">
+          <div class="reward reward-googleplay tw-relative tw-flex tw-justify-center tw-items-center">
+            <div v-if="false" class="tw-absolute badge-failed">
+              Out of stock
+            </div>
             <img src="@/assets/images/rewards/googleplay.png" alt="Google play">
           </div>
           <div class="reward-details tw-flex tw-flex-col tw-justify-center tw-px-3 tw-font-medium tw-text-lg lg:tw-text-xl tw-uppercase tw-tracking-wider">
@@ -62,7 +83,10 @@
           </div>
         </router-link>
         <router-link :to="{ name: 'Home' }" class="reward-item">
-          <div class="reward reward-netflix tw-flex tw-justify-center tw-items-center">
+          <div class="reward reward-netflix tw-relative tw-flex tw-justify-center tw-items-center">
+            <div v-if="false" class="tw-absolute badge-failed">
+              Out of stock
+            </div>
             <img src="@/assets/images/rewards/netflix.png" alt="Netflix">
           </div>
           <div class="reward-details tw-flex tw-flex-col tw-justify-center tw-px-3 tw-font-medium tw-text-lg lg:tw-text-xl tw-uppercase tw-tracking-wider">
@@ -70,11 +94,25 @@
           </div>
         </router-link>
         <router-link :to="{ name: 'Home' }" class="reward-item">
-          <div class="reward reward-spotify tw-flex tw-justify-center tw-items-center">
+          <div class="reward reward-spotify tw-relative tw-flex tw-justify-center tw-items-center">
+            <div v-if="false" class="tw-absolute badge-failed">
+              Out of stock
+            </div>
             <img src="@/assets/images/rewards/spotify.png" alt="Spotify">
           </div>
           <div class="reward-details tw-flex tw-flex-col tw-justify-center tw-px-3 tw-font-medium tw-text-lg lg:tw-text-xl tw-uppercase tw-tracking-wider">
             Spotify
+          </div>
+        </router-link>
+        <router-link :to="{ name: 'Home' }" class="reward-item">
+          <div class="reward reward-discord tw-relative tw-flex tw-justify-center tw-items-center">
+            <div v-if="false" class="tw-absolute badge-failed">
+              Out of stock
+            </div>
+            <img src="@/assets/images/rewards/discord.png" alt="Discord">
+          </div>
+          <div class="reward-details tw-flex tw-flex-col tw-justify-center tw-px-3 tw-font-medium tw-text-lg lg:tw-text-xl tw-uppercase tw-tracking-wider">
+            Discord
           </div>
         </router-link>
       </div>
