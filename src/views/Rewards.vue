35295400037e6a6fc06d12f2ec51269b9c886be0
AdirYed <adir.yed@gmail.com>
2020-11-23 16:21:30 +0200

    rewards page

--- src/views/Rewards.vue
+++ src/views/Rewards.vue
@@ -4,54 +4,157 @@
       <div class="tw-text-primary tw-mb-4 md:tw-mb-2 tw-text-2xl sm:tw-text-3x lg:tw-text-4xl tw-font-thin tw-uppercase tw-tracking-wider">
         Rewards
       </div>
-      <div class="rewards-list tw-flex tw-flex-wrap tw-items-center">
-        <div class="tw-p-3">
+      <div class="rewards-list tw-grid">
+        <router-link :to="{ name: 'Home' }" class="reward-item">
           <div class="reward reward-robux tw-flex tw-justify-center tw-items-center">
-            <img class="img-height" src="../assets/images/rewards/robux.png" alt="robux">
+            <img src="@/assets/images/rewards/robux.png" alt="Robux">
           </div>
-        </div>
-        <div class="tw-p-3">
+          <div class="reward-details tw-flex tw-flex-col tw-justify-between tw-p-4 tw-py-5">
+            <div class="tw-font-medium tw-text-lg lg:tw-text-xl tw-uppercase tw-tracking-wider">
+              Robux
+            </div>
+            <div class="tw-text-sm tw-flex" :class="{ 'tw-text-red-500' : true, 'tw-text-green-500' : false }">
+              <template v-if="true">
+                You need 40 more points
+              </template>
+              <template v-else>
+                You can purchase rewards
+              </template>
+            </div>
+          </div>
+        </router-link>
+        <router-link :to="{ name: 'Home' }" class="reward-item">
           <div class="reward reward-bitcoin tw-flex tw-justify-center tw-items-center">
-            <img src="../assets/images/rewards/bitcoin.png" alt="bitcoin">
+            <img src="@/assets/images/rewards/bitcoin.png" alt="Bitcoin">
           </div>
-        </div>
-        <div class="tw-p-3">
+          <div class="reward-details tw-flex tw-flex-col tw-justify-between tw-p-4 tw-py-5">
+            <div class="tw-font-medium tw-text-lg lg:tw-text-xl tw-uppercase tw-tracking-wider">
+              Bitcoin
+            </div>
+            <div class="tw-text-sm tw-flex" :class="{ 'tw-text-red-500' : true, 'tw-text-green-500' : false }">
+              <template v-if="true">
+                You need 40 more points
+              </template>
+              <template v-else>
+                You can purchase rewards
+              </template>
+            </div>
+          </div>
+        </router-link>
+        <router-link :to="{ name: 'Home' }" class="reward-item">
           <div class="reward reward-apple tw-flex tw-justify-center tw-items-center">
-            <img src="../assets/images/rewards/apple.png" alt="apple">
+            <img src="@/assets/images/rewards/apple.png" alt="Appstore">
           </div>
-        </div>
-        <div class="tw-p-3">
+          <div class="reward-details tw-flex tw-flex-col tw-justify-between tw-p-4 tw-py-5">
+            <div class="tw-font-medium tw-text-lg lg:tw-text-xl tw-uppercase tw-tracking-wider">
+              App Store
+            </div>
+            <div class="tw-text-sm tw-flex" :class="{ 'tw-text-red-500' : true, 'tw-text-green-500' : false }">
+              <template v-if="true">
+                You need 40 more points
+              </template>
+              <template v-else>
+                You can purchase rewards
+              </template>
+            </div>
+          </div>
+        </router-link>
+        <router-link :to="{ name: 'Home' }" class="reward-item">
           <div class="reward reward-xbox tw-flex tw-justify-center tw-items-center">
-            <img src="../assets/images/rewards/xbox.png" alt="xbox">
+            <img src="@/assets/images/rewards/xbox.png" alt="XBOX">
           </div>
-        </div>
-        <div class="tw-p-3">
+          <div class="reward-details tw-flex tw-flex-col tw-justify-between tw-p-4 tw-py-5">
+            <div class="tw-font-medium tw-text-lg lg:tw-text-xl tw-uppercase tw-tracking-wider">
+              XBOX
+            </div>
+            <div class="tw-text-sm tw-flex" :class="{ 'tw-text-red-500' : true, 'tw-text-green-500' : false }">
+              <template v-if="true">
+                You need 40 more points
+              </template>
+              <template v-else>
+                You can purchase rewards
+              </template>
+            </div>
+          </div>
+        </router-link>
+        <router-link :to="{ name: 'Home' }" class="reward-item">
           <div class="reward reward-roblox tw-flex tw-justify-center tw-items-center">
-            <img src="../assets/images/rewards/roblox.png" alt="roblox">
+            <img src="@/assets/images/rewards/roblox.png" alt="Roblox Giftcard">
           </div>
-        </div>
-        <div class="tw-p-3">
+          <div class="reward-details tw-flex tw-flex-col tw-justify-between tw-p-4 tw-py-5">
+            <div class="tw-font-medium tw-text-lg lg:tw-text-xl tw-uppercase tw-tracking-wider">
+              Roblox Giftcard
+            </div>
+            <div class="tw-text-sm tw-flex" :class="{ 'tw-text-red-500' : true, 'tw-text-green-500' : false }">
+              <template v-if="true">
+                You need 40 more points
+              </template>
+              <template v-else>
+                You can purchase rewards
+              </template>
+            </div>
+          </div>
+        </router-link>
+        <router-link :to="{ name: 'Home' }" class="reward-item">
           <div class="reward reward-psn tw-flex tw-justify-center tw-items-center">
-            <img src="../assets/images/rewards/psn.png" alt="psn">
+            <img src="@/assets/images/rewards/psn.png" alt="PSN">
           </div>
-        </div>
-        <div class="tw-p-3">
+          <div class="reward-details tw-flex tw-flex-col tw-justify-between tw-p-4 tw-py-5">
+            <div class="tw-font-medium tw-text-lg lg:tw-text-xl tw-uppercase tw-tracking-wider">
+              PSN
+            </div>
+            <div class="tw-text-sm tw-flex" :class="{ 'tw-text-red-500' : true, 'tw-text-green-500' : false }">
+              <template v-if="true">
+                You need 40 more points
+              </template>
+              <template v-else>
+                You can purchase rewards
+              </template>
+            </div>
+          </div>
+        </router-link>
+        <router-link :to="{ name: 'Home' }" class="reward-item">
           <div class="reward reward-googleplay tw-flex tw-justify-center tw-items-center">
-            <img class="img-height" src="../assets/images/rewards/googleplay.png" alt="googleplay">
+            <img src="@/assets/images/rewards/googleplay.png" alt="Googleplay">
           </div>
-        </div>
-        <div class="tw-p-3">
+          <div class="reward-details tw-flex tw-flex-col tw-justify-between tw-p-4 tw-py-5">
+            <div class="tw-font-medium tw-text-lg lg:tw-text-xl tw-uppercase tw-tracking-wider">
+              Googleplay
+            </div>
+            <div class="tw-text-sm tw-flex" :class="{ 'tw-text-red-500' : true, 'tw-text-green-500' : false }">
+              <template v-if="true">
+                You need 40 more points
+              </template>
+              <template v-else>
+                You can purchase rewards
+              </template>
+            </div>
+          </div>
+        </router-link>
+        <router-link :to="{ name: 'Home' }" class="reward-item">
           <div class="reward reward-netflix tw-flex tw-justify-center tw-items-center">
-            <img src="../assets/images/rewards/netflix.png" alt="netflix">
+            <img src="@/assets/images/rewards/netflix.png" alt="Netflix">
           </div>
-        </div>
+          <div class="reward-details tw-flex tw-flex-col tw-justify-between tw-p-4 tw-py-5">
+            <div class="tw-font-medium tw-text-lg lg:tw-text-xl tw-uppercase tw-tracking-wider">
+              Netflix
+            </div>
+            <div class="tw-text-sm tw-flex" :class="{ 'tw-text-red-500' : true, 'tw-text-green-500' : false }">
+              <template v-if="true">
+                You need 40 more points
+              </template>
+              <template v-else>
+                You can purchase rewards
+              </template>
+            </div>
+          </div>
+        </router-link>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-
 export default {
   name: 'Offerwall',
 }
