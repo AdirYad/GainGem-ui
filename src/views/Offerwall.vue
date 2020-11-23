0b5504637814433bf45fb5edfba5b16cffb7f38d
ortalll20111 <ortalll20111@gmail.com>
2020-11-23 09:38:05 +0200

    change color

--- src/views/Offerwall.vue
+++ src/views/Offerwall.vue
@@ -6,7 +6,7 @@
         More Offers
       </router-link>
     </div>
-    <div class="tw-w-1/2 md:tw-w-1/3 lg:tw-w-1/4 tw-h-12 tw-bg-primary tw-rounded-xl">
+    <div class="miss-point tw-w-1/2 md:tw-w-1/3 lg:tw-w-1/4 tw-h-12 tw-rounded-xl">
       <router-link :to="{name: 'Support'}" class="tw-w-full tw-h-full tw-flex tw-rounded-xl tw-text-secondary tw-uppercase tw-tracking-wider tw-justify-center tw-items-center">
         <fa-icon icon="question" class="tw-h-6 fa-w-40" />
         Missing Points?
