09aee3d068eb773e0a50f374cf93706cecaf882e
AdirYed <adir.yed@gmail.com>
2020-11-25 07:57:26 +0200

    added notifications

---
+++ src/components/Notifications/NotificationsList.vue
@@ -0,0 +1,65 @@
+<template>
+  <teleport to="#notifications">
+    <transition-group name="notifications" tag="div" class="notifications-list tw-fixed tw-bottom-0 tw-right-0">
+      <NotificationMessage v-if="$store.state.notifications.length" v-for="notification in $store.state.notifications"
+                           :key="notification.id"
+                           :notification="notification"
+      />
+    </transition-group>
+  </teleport>
+</template>
+
+<script>
+import NotificationMessage from "@/components/Notifications/NotificationMessage";
+
+export default {
+  name: 'NotificationsList',
+  components: {
+    NotificationMessage,
+  },
+}
+</script>
+
+<style scoped>
+.notifications-list {
+  margin-right: 15px;
+  width: 320px;
+}
+
+@media (max-width: 400px) {
+  .notifications-list {
+    margin-right: 0;
+    width: 100vw;
+
+    .notifications-enter-active {
+      transform: translateX(100vw);
+    }
+
+    .notifications-enter, .notifications-leave-to {
+      transform: translateX(100vw);
+    }
+  }
+}
+
+@media (max-width: 1024px) {
+  .notifications-list {
+    bottom: var(--navigation-height) !important;
+  }
+}
+
+.notifications-leave-active, .notifications-enter-active {
+  opacity: 0;
+}
+
+.notifications-enter-active {
+  transition: none !important;
+  transform: translateX(110%);
+}
+
+.notifications-enter {
+  transform: translateX(-110%);
+}
+.notifications-leave-to {
+  transform: translateX(110%);
+}
+</style>
