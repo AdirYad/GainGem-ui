ae5312aa8975ffdc3175abf17e7114f586d94f60
ortalll20111 <ortalll20111@gmail.com>
2020-11-29 10:50:37 +0200

    add timer - didnt work

--- src/views/Giveaway.vue
+++ src/views/Giveaway.vue
@@ -6,23 +6,19 @@
 
     <div class="tw-p-6 tw-bg-white tw-rounded-sm tw-shadow">
       <div class="tw-text-xl tw-text-primary tw-flex tw-justify-around tw-items-center">
-        <button class="hover:tw-text-secondary tw-text-primary tw-uppercase tw-text-sm tw-tracking-wider tw-font-bold tw-duration-300 tw-border-2 tw-border-primary hover:tw-bg-primary tw-rounded-full tw-px-4 md:tw-px-6 tw-py-2 md:tw-py-3">
+        <button class="tw-uppercase tw-text-sm tw-tracking-wider tw-font-bold tw-duration-300 tw-border-2 tw-text-primary focus:tw-text-secondary hover:tw-text-secondary tw-border-primary focus:tw-bg-primary tw-rounded-full tw-px-4 md:tw-px-6 tw-py-2 md:tw-py-3">
           Enter Giveaway
         </button>
-        <div>
-          <div class="tw-flex tw-rounded-lg tw-border-2 tw-border-primary tw-text-center tw-p-2">
-            00 :
-            59 :
-            59
-          </div>
+        <div class="tw-flex tw-rounded-lg tw-border-2 tw-border-primary tw-text-center tw-p-2">
+            {{ _hour }} : {{ _minutes }} : {{ _seconds % 60 }}
         </div>
-          <div>
-            <div class="tw-text-2xl tw-uppercase tw-tracking-wider tw-font-bold">
-              13 points
-            </div>
-            <div class="tw-text-sm tw-tracking-wider tw-font-thin">
-              Winning Amount
-            </div>
+        <div>
+          <div class="tw-text-2xl tw-uppercase tw-tracking-wider tw-font-bold">
+            13 points
+          </div>
+          <div class="tw-text-sm tw-tracking-wider tw-font-thin">
+            Winning Amount
+          </div>
         </div>
         <div class="tw-text-2xl tw-uppercase tw-tracking-wider tw-font-thin tw-flex tw-items-center">
          15 Users Entered
@@ -72,7 +68,7 @@
       How does it work?
     </div>
     <div class="tw-flex tw-justify-center tw-flex-wrap">
-      <div class="affiliate-description-card tw-w-full tw-flex tw-flex-col tw-items-center tw-w-full xl:tw-w-1/4 tw-bg-secondary tw-border-t-2 tw-border-primary tw-shadow-md tw-p-4 lg:tw-mx-4">
+      <div class="giveaway-description-card tw-w-full tw-flex tw-flex-col tw-items-center tw-w-full xl:tw-w-1/4 tw-bg-secondary tw-border-t-2 tw-border-primary tw-shadow-md tw-p-4 lg:tw-mx-4">
         <div class="tw-flex tw-justify-center tw-items-center tw-w-12 tw-h-12 tw-border-2 tw-border-primary tw-rounded-full tw-text-2xl">
           1
         </div>
@@ -87,13 +83,13 @@
         </div>
       </div>
 
-      <div class="affiliates-arrow">
+      <div class="giveaway-arrow">
         <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="512" height="512" viewBox="0 0 512 512">
           <path id="arrow" d="M32.017,443.987l80.255-187.5L32.017,68.985,480,256.485Z"></path>
         </svg>
       </div>
 
-      <div class="affiliate-description-card tw-w-full tw-flex tw-flex-col tw-items-center tw-w-full xl:tw-w-1/4 tw-bg-secondary tw-border-t-2 tw-border-primary tw-shadow-md tw-p-4 lg:tw-mx-4">
+      <div class="giveaway-description-card tw-w-full tw-flex tw-flex-col tw-items-center tw-w-full xl:tw-w-1/4 tw-bg-secondary tw-border-t-2 tw-border-primary tw-shadow-md tw-p-4 lg:tw-mx-4">
         <div class="tw-flex tw-justify-center tw-items-center tw-w-12 tw-h-12 tw-border-2 tw-border-primary tw-rounded-full tw-text-2xl">
           2
         </div>
@@ -105,13 +101,13 @@
         </div>
       </div>
 
-      <div class="affiliates-arrow">
+      <div class="giveaway-arrow">
         <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="512" height="512" viewBox="0 0 512 512">
           <path id="arrow" d="M32.017,443.987l80.255-187.5L32.017,68.985,480,256.485Z"></path>
         </svg>
       </div>
 
-      <div class="affiliate-description-card tw-w-full tw-flex tw-flex-col tw-items-center tw-w-full xl:tw-w-1/4 tw-bg-secondary tw-border-t-2 tw-border-primary tw-shadow-md tw-p-4 lg:tw-mx-4">
+      <div class="giveaway-description-card tw-w-full tw-flex tw-flex-col tw-items-center tw-w-full xl:tw-w-1/4 tw-bg-secondary tw-border-t-2 tw-border-primary tw-shadow-md tw-p-4 lg:tw-mx-4">
         <div class="tw-flex tw-justify-center tw-items-center tw-w-12 tw-h-12 tw-border-2 tw-border-primary tw-rounded-full tw-text-2xl">
           3
         </div>
@@ -134,57 +130,51 @@ import { useStore } from 'vuex';
 import { ref } from 'vue';
 
 export default {
-  name: 'Invite',
-  setup() {
-    const store = useStore();
+  name: 'Giveaway',
+  props: {
+    hour: {
+      type: Number,
+      default: 1,
+    },
+    minutes: {
+      type: Number,
+      default: 0,
+    },
+    seconds: {
+      type: Number,
+      default: 0,
+    },
+  },
+  setup(props) {
+    let _hour = ref({});
+    let _minutes = ref({});
+    let _seconds = ref({});
 
-    const referral = ref(null);
-    const inviteLink = ref(`${window.location.origin}/?ref_id=${store.state.user.referral_token}`);
-
-    const selectText = (element) => {
-      let range;
-      if (document.selection) {
-        range = document.body.createTextRange();
-        range.moveToElementText(element);
-        range.select();
-      } else if (window.getSelection) {
-        range = document.createRange();
-        range.selectNode(element);
-        window.getSelection().removeAllRanges();
-        window.getSelection().addRange(range);
+    const restart = () => {
+      if(_hour === 0 && _minutes === 0 && _seconds === 0){
+        _hour = props.hour;
+        _minutes = _hour * 60;
+        _seconds = _minutes * 60;
       }
     }
+    let timer = setInterval( countdown(), 1000);
 
-    const unselectText = () => {
-      if (document.selection) {
-        document.selection.empty();
-      } else if (window.getSelection) {
-        window.getSelection().removeAllRanges();
-      }
-    }
-
-    const copyInviteLink = () => {
-      selectText(referral.value);
-      document.execCommand('copy');
-      unselectText();
-
-      store.dispatch('addNotification', {
-        type: 'success',
-        message: `Copied referral link to clipboard`
-      });
-    }
-
+     const countdown = () => {
+      setInterval(_seconds--, 1000);
+      setInterval(_minutes--, 60 * 1000);
+      setInterval(_hour--, 60 * 60 * 1000);
+     }
     return {
-      referral,
-      inviteLink,
-      copyInviteLink,
-    }
+      _seconds,
+      _minutes,
+      _hour,
+    };
   }
 }
 </script>
 
 <style scoped>
-.affiliates-arrow {
+.giveaway-arrow {
   max-width: 100px;
   fill: var(--primary-color);
 
@@ -197,21 +187,21 @@ export default {
 }
 
 @media (min-width: 1024px) {
-  .affiliate-description-card {
+  .giveaway-description-card {
     max-width: 300px;
   }
 
-  .affiliate-description-card {
+  .giveaway-description-card {
     flex: 3;
   }
 
-  .affiliates-arrow {
+  .giveaway-arrow {
     flex: 1;
   }
 }
 
 @media (max-width: 1023px) {
-  .affiliates-arrow {
+  .giveaway-arrow {
     transform: rotate(90deg);
     max-width: 40px;
     margin: 15px 0;
