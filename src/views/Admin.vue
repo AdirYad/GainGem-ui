fd37a758283f10127bad455bd1b3f4c5b9e5f17f
AdirYed <adir.yed@gmail.com>
2020-12-25 17:34:47 +0200

    test

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -2,31 +2,50 @@
   <div>
     <div class="tw-flex tw-flex-col lg:tw-flex-row">
       <div class="tw-flex tw-flex-1 tw-bg-white tw-shadow-md">
-<!--        <Tabs class="tw-min-h-12 tw-w-full">-->
-<!--          <Tab name="Reward Stock" query="stock">-->
-<!--            <RewardStock />-->
-<!--          </Tab>-->
-<!--          <Tab name="Promo Codes" query="promo-codes">-->
-<!--            <PromoCodes />-->
-<!--          </Tab>-->
-<!--          <Tab name="Users" query="users">-->
-<!--            <Users />-->
-<!--          </Tab>-->
-<!--          <Tab name="Announcement Banner" query="announcement-banner">-->
-<!--            <AnnouncementBanner />-->
-<!--          </Tab>-->
-<!--        </Tabs>-->
-        dad
+        <Tabs class="tw-min-h-12 tw-w-full">
+          <Tab name="Account Details" query="details">
+            <Details />
+          </Tab>
+          <Tab name="Transactions" query="transactions">
+            <Transactions />
+          </Tab>
+          <Tab name="Activity" query="activity">
+            <Activity />
+          </Tab>
+          <Tab name="Referrals" query="referrals">
+            <Referrals />
+          </Tab>
+        </Tabs>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import Tab from '@/components/Tab'
+import Tabs from "@/components/Tabs";
+import RewardStock from '@/components/Admin/RewardStock';
+import PromoCodes from '@/components/Admin/PromoCodes';
+import Users from '@/components/Admin/Users';
+import AnnouncementBanner from '@/components/Admin/AnnouncementBanner';
+import Details from "@/components/Profile/Details";
+import Transactions from "@/components/Profile/Transactions";
+import Activity from "@/components/Profile/Activity";
+import Referrals from "@/components/Profile/Referrals";
 
 export default {
   name: 'Admin',
   components: {
+    Tab,
+    Tabs,
+    RewardStock,
+    PromoCodes,
+    Users,
+    AnnouncementBanner,
+    Details,
+    Transactions,
+    Activity,
+    Referrals,
   },
 }
 </script>
