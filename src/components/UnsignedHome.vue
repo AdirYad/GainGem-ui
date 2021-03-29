5a17186550c4475f593b666559ff821520c5656e
AdirYad <adir.yed@gmail.com>
2021-03-29 22:27:36 +0300

    fixes

--- src/components/UnsignedHome.vue
+++ src/components/UnsignedHome.vue
@@ -29,7 +29,7 @@
     <div class="tw-flex tw-justify-around tw-items-center tw-text-center tw-py-4">
       <div class="tw-mr-2 lg:tw-mr-0 tw-w-1/2 lg:tw-w-1/4">
         <div class="tw-text-primary tw-text-2xl sm:tw-text-4xl lg:tw-text-5xl tw-font-bold">
-          <AnimatedNumber :number="parseFloat($store.state.stats.total_points_earned)" />
+          <AnimatedNumber :number="parseFloat($store.state.stats.total_points_earned)" :duration="4" />
         </div>
         <div class="tw-flex tw-justify-center tw-items-center tw-text-xs md:tw-text-sm tw-font-light tw-uppercase sm:tw-tracking-wider">
           <fa-icon class="tw-h-6 fa-w-20" icon="coins" />
@@ -40,7 +40,7 @@
       </div>
       <div class="tw-w-1/2 lg:tw-w-1/4">
         <div class="tw-text-primary tw-text-2xl sm:tw-text-4xl lg:tw-text-5xl tw-font-bold">
-          <AnimatedNumber :number="parseInt($store.state.stats.total_offers_completed)" />
+          <AnimatedNumber :number="parseInt($store.state.stats.total_offers_completed)" :duration="4" />
         </div>
         <div class="tw-flex tw-justify-center tw-items-center tw-text-xs md:tw-text-sm tw-font-light tw-uppercase sm:tw-tracking-wider">
           <fa-icon class="tw-h-6 fa-w-20" :icon="['fab', 'buffer']" />
