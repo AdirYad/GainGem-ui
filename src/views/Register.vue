6e90e27ee094e8294db60517c6cd48a13f4d46b8
AdirYad <adir.yed@gmail.com>
2021-04-07 23:04:46 +0300

    added register notice

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -45,9 +45,9 @@
           Password
         </label>
         <input id="password" type="password" placeholder="******************"
-               class="input tw-duration-300 tw-shadow tw-appearance-none tw-border tw-rounded tw-w-full tw-py-2 tw-px-3 tw-text-gray-500 tw-leading-tight focus:tw-outline-none"
+               class="input tw-duration-300 tw-shadow tw-appearance-none tw-border tw-rounded tw-w-full tw-py-2 tw-px-3 tw-text-gray-500 tw-leading-tight focus:tw-outline-none tw-mb-3"
                v-model="auth.password"
-               :class="{ 'input-invalid tw-mb-3' : v$.password.$invalid || errors.password }"
+               :class="{ 'input-invalid' : v$.password.$invalid || errors.password }"
                @keydown="resetErrors('password')"
         >
         <p v-if="v$.password.$error" class="tw-text-red-500 tw-text-xs tw-italic">
@@ -55,6 +55,9 @@
         </p>
         <p v-else-if="errors.password" class="tw-text-red-500 tw-text-xs tw-italic">
           {{ errors.password[0] }}
+        </p>
+        <p v-else class="tw-text-xs tw-italic">
+          We recommend using a unique password for your EzRewards account
         </p>
       </div>
       <div class="tw-mb-4">
